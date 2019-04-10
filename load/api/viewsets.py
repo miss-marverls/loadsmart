7f4df9b32c8a392675870056730be71b1dcedd26
Mayara <mayaradiasg@gmail.com>
2019-04-09 21:13:37 -0300

    implemented accept a load on carrier API
    The carrier API now allows carriers to accept a load using the url:
    http://localhost:8000/load/api/carrier/*ID*/accept/
    where *ID* is the load's id.
    When used, this url sets a load's carrier field to the logged user id.
    The carrier needs to be logged to accept a load.

--- load/api/viewsets.py
+++ load/api/viewsets.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 from load.models import Load
 from load.api.serializers import LoadSerializer
 
@@ -42,3 +44,12 @@ class CarrierLoadViewSet(ModelViewSet):
         queryset = Load.objects.filter(carrier=None)
         serializer = LoadSerializer(queryset, many=True)
         return Response(serializer.data)
+
+    @action(methods=['get', 'post'], detail=True)
+    def accept(self, request, pk=None):
+        load = get_object_or_404(Load, pk=pk, carrier=None)
+        serializer = LoadSerializer(load, data={'carrier': request.user.id}, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
