21f804675465213ca9cd8c747edebc5bc9f37634
denise.alima <denise.developer@gmail.com>
2019-04-18 17:00:47 -0300

    Add date picker on new load

--- load/forms.py
+++ load/forms.py
@@ -3,10 +3,18 @@ from .models import Load
 from bootstrap_modal_forms.forms import BSModalForm
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class LoadForm(BSModalForm):
     class Meta:
         model = Load
-        fields = ('pickup_date', 'ref', 'origin_city', 'destination_city', 'shipper_price',)
+        fields = ('pickup_date', 'ref', 'origin_city',
+                  'destination_city', 'shipper_price',)
+        widgets = {
+            'pickup_date': DateInput(),
+        }
 
 
 class LoadEditRateForm(BSModalForm):
