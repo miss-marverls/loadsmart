e8541e4ad321c4ebd5c3e11903c0b3563bf85813
Mayara <mayaradiasg@gmail.com>
2019-04-19 18:23:27 -0300

    Refactor permissions
    Removed duplicateds permissions.

--- load/api/permissions.py
+++ load/api/permissions.py
@@ -3,9 +3,9 @@ from rest_framework.permissions import BasePermission
 
 class CanAccess(BasePermission):
     def has_permission(self, request, view):
-        if view.action in ['accept', 'reject', 'rejected', 'carrier_available', 'carrier_accepted']:
+        if view.action in ['accept', 'reject', 'rejected']:
             return request.user.is_carrier
-        elif view.action in ['create', 'shipper_available', 'shipper_accepted', 'list']:
+        elif view.action in ['create', 'list']:
             return request.user.is_shipper
         elif view.action in ['available', 'accepted']:
             return True
