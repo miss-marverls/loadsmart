d2033c91c9ee5f5198cb38c86ac39bec0454e923
Mayara <mayaradiasg@gmail.com>
2019-04-22 17:48:42 -0300

    prepare for deploy

--- loadsmart/settings.py
+++ loadsmart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 AUTH_USER_MODEL = "users.Shipper"
 LOGIN_REDIRECT_URL = '/'
@@ -58,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'loadsmart.urls'
@@ -125,6 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Use nose to run all tests
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
@@ -136,3 +144,5 @@ NOSE_ARGS = [
     '--cover-erase',
     '--cover-html',
 ]
+
+django_heroku.settings(locals())
