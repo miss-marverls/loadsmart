c5af4a95d9de23f51ba201a496b91544f3652e20
Mayara <mayaradiasg@gmail.com>
2019-05-01 19:36:35 -0300

    Set debug mode as False on settings.py

--- loadsmart/settings.py
+++ loadsmart/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_8^t*5@sz_!igjdqunk$%cywv9hr417++35*)fsz6!rq@-h#45'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Application definition
 
