14284dbdc0195c5a513356e701ef7103a6898c45
Mayara <mayaradiasg@gmail.com>
2019-05-01 23:27:21 -0300

    Revert "Add logging settings"
    This reverts commit e9f742fc25f7065e1fcf00e84f17c31f6d7e8a21.

--- loadsmart/settings.py
+++ loadsmart/settings.py
@@ -157,19 +157,3 @@ NOSE_ARGS = [
 ]
 
 django_heroku.settings(locals())
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
