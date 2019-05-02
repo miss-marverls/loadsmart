ed2c918d6aea506a0443e662ae90c6bb3b06d09f
Mayara <mayaradiasg@gmail.com>
2019-05-02 01:16:52 -0300

    Add database configurations to deploy

--- loadsmart/settings.py
+++ loadsmart/settings.py
@@ -85,10 +85,21 @@ WSGI_APPLICATION = 'loadsmart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dfdckpudrchqoh',
+        'USER': 'bucsiyadnnvhty',
+        'PASSWORD': '6d4e3b81741d1e5205aea58f1947213efc3d326a01ac2f64c1ca188ba28b1d8c',
+        'HOST': 'ec2-54-225-116-36.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
